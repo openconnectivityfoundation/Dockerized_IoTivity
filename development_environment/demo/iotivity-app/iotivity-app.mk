488188de1c5f5c80386f99821eca885d1a409fbd
Andy Dolan <20274393+adolan5@users.noreply.github.com>
2021-03-26 08:35:25 -0600

    Update example app makefile

--- development_environment/demo/iotivity-app/iotivity-app.mk
+++ development_environment/demo/iotivity-app/iotivity-app.mk
@@ -1,9 +1,16 @@
+# To include iotivity-lite/port/linux/Makefile which has important variables defined
 include Makefile
 
-iotivity-app/speaker_server: iotivity-app/speaker_server.c libiotivity-lite-server.a
+# Full target path in container will be /iotivity-lite/port/linux/iotivity-app
+# This Makefile is referred to from the /iotivity-lite/port/linux directory, so
+# it references relative paths
+APP_TARGET=iotivity-app/speaker_server
+
+$(APP_TARGET): iotivity-app/speaker_server.c libiotivity-lite-server.a
 	@mkdir -p $@_creds
 	${CC} -o $@ $< libiotivity-lite-server.a -DOC_SERVER ${CFLAGS} ${LIBS}
 
+# Optional "clean" routine that doesn't override the ioitivity-lite clean
 .PHONY: cleanbuild
 cleanbuild:
-	rm -rf iotivity-app/speaker_server iotivity-app/speaker_server_creds
+	rm -rf $(APP_TARGET) $(APP_TARGET)_creds

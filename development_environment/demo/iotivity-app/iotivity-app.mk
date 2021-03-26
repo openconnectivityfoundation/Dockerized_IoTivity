447adb7a829e3ea22a7c77c5173e929b25ec6273
Andy Dolan <20274393+adolan5@users.noreply.github.com>
2021-03-26 08:39:37 -0600

    Update comments in Makefiles

--- development_environment/demo/iotivity-app/iotivity-app.mk
+++ development_environment/demo/iotivity-app/iotivity-app.mk
@@ -1,9 +1,12 @@
+# The ocfadmin/iotivity-builder container assumes that this file will be named
+# "iotivity-app.mk" and will be located in /iotivity-app (as a bind mount or a volume)
+# This Makefile is referred to from the /iotivity-lite/port/linux directory, so
+# it accordingly references relative paths
+
 # To include iotivity-lite/port/linux/Makefile which has important variables defined
 include Makefile
 
 # Full target path in container will be /iotivity-lite/port/linux/iotivity-app
-# This Makefile is referred to from the /iotivity-lite/port/linux directory, so
-# it references relative paths
 APP_TARGET=iotivity-app/speaker_server
 
 $(APP_TARGET): iotivity-app/speaker_server.c libiotivity-lite-server.a
